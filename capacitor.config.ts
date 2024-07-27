d6a9552bd41ed124f1461e3c1792ae69e618a2c5
GenericOctopus <genericoctopus@gmail.com>
2024-07-27 00:03:31 -0230

    init commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ion.react.todo',
+  appName: 'ion-react-todo',
+  webDir: 'dist'
+};
+
+export default config;
